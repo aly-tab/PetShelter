847eefacb0b25dfef65069a9ae7cf94d3b0b7218
w-phil <wphillips892@gmail.com>
2022-03-12 10:34:35 -0500

    deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('./server/config/mongoose.config');
 const cors = require('cors');
-app.use(cors({origin: process.env.REACT_URI}));
+app.use(cors({credentials: true, origin: process.env.REACT_URI}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
